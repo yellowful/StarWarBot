f3628693afd913e6791d2b9dbcaf36a42d73d4f8
yellowful <richenyou@gmail.com>
2020-04-05 18:29:48 +0800

    Updates

---
+++ precache-manifest.68e0085e4faf74bb76507ca984cb8d01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f2205c7562b4c6722bf41b1a5d25fe",
+    "url": "/StarWarBot/index.html"
+  },
+  {
+    "revision": "141b5c0418d751eb6e2e",
+    "url": "/StarWarBot/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a4b4bca772ebd5665a8d",
+    "url": "/StarWarBot/static/css/main.9d3cfe72.chunk.css"
+  },
+  {
+    "revision": "141b5c0418d751eb6e2e",
+    "url": "/StarWarBot/static/js/2.79fbd333.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/StarWarBot/static/js/2.79fbd333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b4bca772ebd5665a8d",
+    "url": "/StarWarBot/static/js/main.eb5ef81a.chunk.js"
+  },
+  {
+    "revision": "4262d10948fc97ffb534",
+    "url": "/StarWarBot/static/js/runtime-main.d08024af.js"
+  },
+  {
+    "revision": "48ccec714314d09f6ed77cba46219512",
+    "url": "/StarWarBot/static/media/SWATRG__.48ccec71.woff"
+  }
+]);
